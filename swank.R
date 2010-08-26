b7cfe0a0e9f9456e13c680cce0e302ee4c6c210a
Christophe Rhodes <csr21@cantab.net>
2010-08-26 11:36:30 +0100

    Add licence information to swank.R
    Same terms as R itself: GNU GPLv2 or later version

--- swank.R
+++ swank.R
@@ -1,3 +1,18 @@
+### This program is free software; you can redistribute it and/or
+### modify it under the terms of the GNU General Public Licence as
+### published by the Free Software Foundation; either version 2 of the
+### Licence, or (at your option) any later version.
+###
+### This program is distributed in the hope that it will be useful,
+### but WITHOUT ANY WARRANTY; without even the implied warranty of
+### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+### GNU General Public Licence for more details.
+###
+### A copy of version 2 of the GNU General Public Licence is available
+### at <http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt>; the
+### latest version of the GNU General Public Licence is available at
+### <http://www.gnu.org/licenses/gpl.txt>.
+
 swank <- function(port=4005) {
   acceptConnections(port, FALSE)
 }
